a379a4c143c0520fc0d281cab14a1632f0be13d7
ronair01 <33083080+ronair01@users.noreply.github.com>
2018-10-23 11:27:17 +0530

    first commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=soltestv1-Web-Deploy.pubxml -p:Password=br94tNalfQRB8FqToBTPkoBW7m5lG2Jsu0CNn37FSqYak8BRQcabsKPqq3DY
+
